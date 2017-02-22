80ce187afedfc453101bdc2fc9aedff179258e42
pleasure <pleasure@cats-whiskers.mit.edu>
2017-02-22 13:37:05 -0500

    url fix

--- pleasure_app/views.py
+++ pleasure_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 from .models import Educator
+from .models import Presentation
 
 # Create your views here.
 def index(request):
@@ -12,5 +13,16 @@ def index(request):
 		'educator_list':educator_list,
 	}
 	return HttpResponse(template.render(context, request))
+
+def scheduling(request):
+	educator_list = Educator.objects.all()
+	presentation_list = Presentation.objects.all()
+	template = loader.get_template('pleasure_app/scheduling.html')
+	context = {
+		'educator_list':educator_list,
+		'presentation_list':presentation_list,
+	}
+	return HttpResponse(template.render(context, request))
+	
 
 
